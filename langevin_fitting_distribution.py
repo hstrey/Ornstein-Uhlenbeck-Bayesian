fafc0d97fa22f5478829d75c394493da4adf1391
hstrey <Helmut.Strey@gmail.com>
2016-04-25 16:56:51 -0400

    tested difference between RK and Euler - seems to give the same result

--- langevin_fitting_distribution.py
+++ langevin_fitting_distribution.py
@@ -6,14 +6,25 @@ from lmfit.models import ExponentialModel
 import pandas as pd
 from itertools import accumulate
 
-# differential equation x_i = x_(i-1) - k/gamma*x_(i-1) + sqrt(2*D*delta_t)*w_i
-def next_point(x,y):
-    return x - k/gamma*x*delta_t + ampl*y
-
 k,gamma,D = 1.0,1.0,1.0
 delta_t=0.01
 ampl = np.sqrt(2*D*delta_t)
-N=1000
+a=k/gamma*delta_t
+
+# differential equation x_i = x_(i-1) - k/gamma*x_(i-1) + sqrt(2*D*delta_t)*w_i
+def next_point_euler(x,y):
+    return x - a*x + ampl*y
+    
+# differential equation x_i = x_(i-1) - k/gamma*x_(i-1) + sqrt(2*D*delta_t)*w_i
+# using 4th-order Runge-Kutta
+def next_point_RK4(x,y):
+    k0=-a*x
+    k1=-a*(x+k0/2.0)
+    k2=-a*(x+k1/2.0)
+    k3=-a*(x+k2)
+    return x + (k0+2*k1+2*k2+k3)/6 + ampl*y
+
+N=5000
 M=10000
 t_list=[]
 A_list=[]
@@ -21,16 +32,18 @@ mean_list=[]
 std_list=[]
 mod = ExponentialModel()
 acf_avg=np.zeros(N)
+acf_std=np.zeros(N)
 for i in range(M):
     # random force
     w=np.random.normal(0,1,N)
-    x = np.fromiter(accumulate(w, next_point),np.float)
+    x = np.fromiter(accumulate(w, next_point_euler),np.float)
 
     # see http://docs.scipy.org/doc/scipy-0.15.1/reference/generated/scipy.signal.fftconvolve.html
     autocorr = signal.fftconvolve(x, x[::-1], mode='full')
     n=len(autocorr)
     autocorr=autocorr[int((n-1)/2):]*2.0/(n+1)
     acf_avg=acf_avg+autocorr
+    acf_std=acf_std+autocorr**2
     y = autocorr[:200]
     t = np.arange(200)
 
@@ -44,15 +57,17 @@ for i in range(M):
     print('mean: ',x.mean(),'std: ',x.std(),'amplitude: ',out.values['amplitude'],'decay: ',out.values['decay'])
 
 acf_avg=acf_avg/M
-y = acf_avg[:500]
-t = np.arange(500)
+acf_std=np.sqrt((acf_std-acf_avg**2)/M)
+y = acf_avg[:N]
+dy=acf_std[:N]
+t = np.arange(N)
 
 pars = mod.guess(y, x=t)
-out  = mod.fit(y, pars, x=t)
+out  = mod.fit(y, pars, x=t, weights=1./dy)
 print(out.fit_report(min_correl=0.25))
 
 plt.figure()
-plt.plot(t,y,"o")
+plt.errorbar(t,y,yerr=dy,fmt="ob")
 plt.plot(t,out.best_fit)
 
 t_list=np.array(t_list)
