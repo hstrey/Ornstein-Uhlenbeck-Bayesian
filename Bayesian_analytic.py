231e938c3896ced1a3a32f418f47a91080d0d7d6
hstrey <Helmut.Strey@gmail.com>
2016-08-31 17:45:18 -0400

    implemented new analytic analysis but it only works for D.  Need to work on my math

--- Bayesian_analytic.py
+++ Bayesian_analytic.py
@@ -16,7 +16,7 @@ def myfunction(t,amp,D):
 
 MyModel=lm.Model(myfunction)
 
-results_dir='results/data100/'
+results_dir='results/delta10-2/data2/'
 N=100 # data length that is used for analysis
 M=250 # number of points of the autocorrelation function that is used for fitting
 delta_t=0.01
@@ -24,43 +24,43 @@ data=pd.read_csv(results_dir+"data.csv")
 data_length=len(data)
 samples=int(data_length / N)
 
-amplitude=[]
-amplitude_var=[]
-diffusion=[]
-diffusion_var=[]
-
-a=[]
-
-v_sum=0 # sum of (x_i+1 - x_i)**2
-
-a_sum=0 # sum of (x_i+1 - x_i)*x_i
+alpha_A=[2.1]
+beta_A=[1.1]
+alpha_D=[2.1]
+beta_D=[1.1]
 
 for i in range(samples):
-    if i<samples-1:
-        x=data['x'][N*i:N*(i+1)+1]
-        nu=N*(i+1)
-    else:
-        x = data['x'][N * i:N * (i + 1)]
-        nu=data_length-1
+    x = np.array(data['x'][N * i:N * (i + 1)])
+
     d=np.diff(x) # calculate the difference
-    v_sum=v_sum+np.sum(d**2)
+    v_sum=np.sum(d**2)
 
-    a_sum=a_sum+np.sum(d*x[:-1])
+    a_sum=np.sum(d*x[:-1])+x[0]**2
 
-    a.append(a_sum/nu)
+    alpha_D.append(alpha_D[-1]+(N-1)/2.0)
+    beta_D.append(beta_D[-1]+v_sum/4.0/delta_t)
 
-    v=v_sum/nu
-    mean=nu/(nu-2)*v
-    var=2*nu**2/(nu-2)**2/(nu-4)*v**2
+    alpha_A.append(alpha_A[-1]+0.5)
+    beta_A.append(beta_A[-1]+a_sum/2.0)
 
-    diffusion.append(mean/2/delta_t)
-    diffusion_var.append(np.sqrt(var)/2/delta_t)
+alpha_A=np.array(alpha_A)
+beta_A=np.array(beta_A)
+
+alpha_D=np.array(alpha_D)
+beta_D=np.array(beta_D)
+
+diffusion=beta_D/(alpha_D-1.0)
+diffusion_var=beta_D**2/(alpha_D-2.0)/(alpha_D-1.0)**2
+
+ampl=beta_A/(alpha_A-1.0)
+ampl_var=beta_A**2/(alpha_A-2.0)/(alpha_A-1.0)**2
+
+resultdict= dict(mean_A=ampl, std_A=np.sqrt(ampl_var), mean_D=diffusion, std_D=np.sqrt(diffusion_var))
+
+df=pd.DataFrame(resultdict)
+df.to_csv(results_dir+'results_analytic'+str(N)+'.csv',index=False)
 
 n=np.arange(len(diffusion))
-
-d=np.linspace(0.8,1.2,100)
-pd=invgamma.pdf(d,nu/2.0,scale=nu*v/4.0/delta_t)
-print(pd)
 
 plt.figure()
 plt.title('diffusion')
@@ -75,16 +75,16 @@ plt.xlabel('iterations')
 plt.ylabel('D var')
 
 plt.figure()
-plt.title('D distribution')
-plt.plot(d,pd)
-plt.xlabel('d')
-plt.ylabel('p(d)')
+plt.title('Amplitude')
+plt.errorbar(n,ampl,yerr=ampl_var,fmt="o")
+plt.xlabel('A')
+plt.ylabel('iterations')
 
 plt.figure()
-plt.title('a')
-plt.plot(a)
+plt.title('A Variance')
+plt.plot(n,ampl_var,"o")
 plt.xlabel('iterations')
-plt.ylabel('a')
+plt.ylabel('A var')
 
 plt.show()
 
