c793edd2f5da8e3b846597fff8307eb22486fa2f
hstrey <Helmut.Strey@gmail.com>
2016-09-25 09:08:47 -0400

    cleaned up repository and removed all files that are outdated.  Could have probably cleaned more

--- repeat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in `seq 1 $1`;
-do
-    time python langevin_pymc3_repeat.py >> log.txt
-done
--- repeat50.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in `seq 1 $1`;
-do
-    theano-cache clear
-    time python langevin_pymc3_repeat50.py >> log50.txt
-done
